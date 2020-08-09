2010687db08378f30b71e166dd892ccdf8b3e412
Henrique Akao <henriquetanioka@gmail.com>
2020-08-09 01:49:58 -0300

    v1

---
+++ Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace ex15.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using ex15.Entities;
+
+namespace ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string sourcePath = Console.ReadLine();
+
+            try
+            {
+                string[] source = File.ReadAllLines(sourcePath);
+                string targetFolder = Path.GetDirectoryName(sourcePath) + @"\out";
+                string targetPath = targetFolder + @"\summary.csv";
+
+
+                Directory.CreateDirectory(targetFolder);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string s in source)
+                    {
+                        string[] info = s.Split(',');
+                        string name = info[0];
+                        double price = double.Parse(info[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(info[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product.Name + "," + product.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
